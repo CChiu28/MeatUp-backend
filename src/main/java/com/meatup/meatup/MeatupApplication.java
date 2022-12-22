d721ee0770fd97ad96fda223d618d65260a51daf
David Chiu <ScorpiusDX@gmail.com>
2022-12-21 19:01:52 -0500

    start spring boot

---
+++ src/main/java/com/meatup/meatup/MeatupApplication.java
@@ -0,0 +1,13 @@
+package com.meatup.meatup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeatupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeatupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meatup/meatup/MeatupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meatup.meatup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeatupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
